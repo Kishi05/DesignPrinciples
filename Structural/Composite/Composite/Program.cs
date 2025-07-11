35e0daec6d5671fd5b37a192a32fbb6ec264cf0f
Kishore <a.kishorekumar01@gmail.com>
2025-07-12 02:36:00 +0530

    Code Cleanup

--- Structural/Composite/Composite/Program.cs
+++ Structural/Composite/Composite/Program.cs
@@ -7,17 +7,14 @@ employee1.Add(dependent1);
 
 //Employee 2
 Employee employee2 = new Employee(4, "Jack", "Mr", 30, "Self");
-Dependent dependent2 = new Dependent(6, "Rose", "Mrs", 30, "Spouse");
-Dependent dependent2_1 = new Dependent(7, "Mary", "Mrs", 55, "Mother");
-Dependent dependent2_2 = new Dependent(9, "John", "Master", 1, "Child");
-employee2.Add(dependent2);
-employee2.Add(dependent2_1);
-employee2.Add(dependent2_2);
+employee2.Add(new Dependent(6, "Rose", "Mrs", 30, "Spouse"));
+employee2.Add(new Dependent(7, "Mary", "Mrs", 30, "Mother"));
+employee2.Add(new Dependent(9, "John", "Master", 1, "Child"));
 
 //Adding to Dependency Chart
-DependencyChart dependencyChart = new DependencyChart();
-dependencyChart.AddRoot(employee1);
-dependencyChart.AddRoot(employee2);
+DependencyChart chart = new DependencyChart();
+chart.AddRoot(employee1);
+chart.AddRoot(employee2);
 
 //Print
-dependencyChart.PrintChart();
+chart.PrintChart();
