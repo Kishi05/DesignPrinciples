22a817a14031350734999ad7359895befe2b4a12
Kishore <a.kishorekumar01@gmail.com>
2025-07-12 19:20:55 +0530

    Design Pattern - Structural - Decorator - Core logic

---
+++ Structural/Decorator/DecoratorTry/Decorator.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Decorator
+{
+    interface ICoffee
+    {
+        string GetDescription();
+        double GetCost();
+    }
+
+    class SimpleCoffee : ICoffee
+    {
+        public string GetDescription() => "Simple Coffee";
+        public double GetCost() => 100;
+    }
+
+    // Decorator base
+    abstract class CoffeeDecorator : ICoffee
+    {
+        protected ICoffee coffee;
+        public CoffeeDecorator(ICoffee coffee)
+        {
+            this.coffee = coffee;
+        }
+        public virtual string GetDescription() => coffee.GetDescription();
+        public virtual double GetCost() => coffee.GetCost();
+    }
+
+    // Concrete decorators
+    class Milk : CoffeeDecorator
+    {
+        public Milk(ICoffee coffee) : base(coffee) { }
+        public override string GetDescription() => coffee.GetDescription() + ", Milk";
+        public override double GetCost() => coffee.GetCost() + 20;
+    }
+
+    class Sugar : CoffeeDecorator
+    {
+        public Sugar(ICoffee coffee) : base(coffee) { }
+        public override string GetDescription() => coffee.GetDescription() + ", Sugar";
+        public override double GetCost() => coffee.GetCost() + 10;
+    }
+
+}
---
+++ Structural/Decorator/DecoratorTry/Program.cs
@@ -0,0 +1,8 @@
+﻿using Decorator;
+
+ICoffee coffee = new SimpleCoffee();
+coffee = new Milk(coffee);
+coffee = new Sugar(coffee);
+
+Console.WriteLine(coffee.GetDescription()); // "Simple Coffee, Milk, Sugar"
+Console.WriteLine(coffee.GetCost());        // 130
